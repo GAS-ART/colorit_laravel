7b7af55c5dc8bc6e079501121bb84597ff797747
Artem <temoha1386@gmail.com>
2022-03-31 15:33:05 +0300

    Added products block to polygraphy page

--- resources/views/polygraphy.blade.php
+++ resources/views/polygraphy.blade.php
@@ -32,7 +32,7 @@
    </div>
    <h3 class="outdor-advertising-about__tittle">Colorit agency</h3>
 </section>
-<section class="smm-to-top">
+<section class="smm-to-top polygraphy-to-top">
    <div class="smm-to-top__container">
       <div class="smm-to-top__tittle tittle">@lang('polygraphy.relevance.tittle')</div>
       <div class="smm-to-top__item">
@@ -49,5 +49,207 @@
       </div>
    </div>
 </section>
+<section class="outdor-advertising-products">
+   <div class="outdor-advertising-products__container">
+      <div class="polygraphy-products-tittle tittle">
+         @lang('polygraphy.products.tittle')
+      </div>
+   </div>
+   <div class="outdor-advertising-products__container">
 
+      <div class="outdor-advertising-products__item item-outdor-advertising-products">
+         <div class="item-outdor-advertising-products__img">
+            <img src="{{asset('img/outdoor_advertising/letters.jpg')}}" alt="">
+         </div>
+         <div class="item-outdor-advertising-products__description description-product">
+            <h2 class="description-product__tittle">
+               @lang('polygraphy.products.business_cards.tittle')</h2>
+            <div class="description-product__body">
+               <div class="description-product__text">
+                  @lang('polygraphy.products.business_cards.text')
+               </div>
+               <div class="description-product__type">@lang('polygraphy.products.business_cards.types')</div>
+               <ul class="description-product__list products-list-read-more">
+                  <li class="description-product__item">@lang('polygraphy.products.business_cards.item')</li>
+                  <li class="description-product__item">@lang('polygraphy.products.business_cards.item1')</li>
+                  <li class="description-product__item">@lang('polygraphy.products.business_cards.item2')</li>
+                  <li class="description-product__item description-product__text-more">
+                     @lang('polygraphy.products.business_cards.item3')</li>
+                  <li class="description-product__item description-product__text-more">
+                     @lang('polygraphy.products.business_cards.item4')</li>
+                  <li class="description-product__item description-product__text-more">
+                     @lang('polygraphy.products.business_cards.item5')</li>
+                  <li class="description-product__item description-product__text-more">
+                     @lang('polygraphy.products.business_cards.item6')</li>
+                  <li class="description-product__item description-product__text-more">
+                     @lang('polygraphy.products.business_cards.item7')</li>
+                  <li class="description-product__item description-product__text-more">
+                     @lang('polygraphy.products.business_cards.item8')</li>
+               </ul>
+               <button class="description-product__read-more-btn">@lang('outdorAdvertising.products.read_more')</button>
+               <div class="description-product__read-more">
+                  <div class="description-product__text-more-mobile">
+                     @lang('polygraphy.products.business_cards.text_more')
+                  </div>
+                  <div class="description-product__details">
+                     @lang('polygraphy.products.business_cards.details')
+                  </div>
+               </div>
+               <button class="description-product__read-less-btn">@lang('outdorAdvertising.products.read_less')</button>
+               <button class="description-product__btn popup-btn">@lang('main.order')</button>
+            </div>
+         </div>
+         <button
+            class="item-outdor-advertising-products__btn-mobile description-product__btn popup-btn">@lang('main.order')</button>
+      </div>
+
+      <div class="outdor-advertising-products__item item-outdor-advertising-products">
+         <div class="item-outdor-advertising-products__description description-product">
+            <h2 class="description-product__tittle"> @lang('polygraphy.products.flyers.tittle')</h2>
+            <div class="description-product__body">
+               <div class="description-product__text">
+                  @lang('polygraphy.products.flyers.text')
+               </div>
+               <div class="description-product__details">
+                  @lang('polygraphy.products.flyers.details')
+               </div>
+               <button class="description-product__btn popup-btn">@lang('main.order')</button>
+            </div>
+         </div>
+         <div class="item-outdor-advertising-products__img">
+            <img src="{{asset('img/outdoor_advertising/neon_signs.jpg')}}" alt="">
+         </div>
+         <button
+            class="item-outdor-advertising-products__btn-mobile description-product__btn popup-btn">@lang('main.order')</button>
+      </div>
+
+      <div class="outdor-advertising-products__item item-outdor-advertising-products">
+         <div class="item-outdor-advertising-products__img">
+            <img src="{{asset('img/outdoor_advertising/led_screen.jpg')}}" alt="">
+         </div>
+         <div class="item-outdor-advertising-products__description description-product">
+            <h2 class="description-product__tittle">
+               @lang('polygraphy.products.booklet.tittle')</h2>
+            <div class="description-product__body">
+               <div class="description-product__text">
+                  @lang('polygraphy.products.booklet.text')
+               </div>
+               <div class="description-product__details">
+                  @lang('polygraphy.products.booklet.details')
+               </div>
+               <button class="description-product__btn popup-btn">@lang('main.order')</button>
+            </div>
+         </div>
+         <button
+            class="item-outdor-advertising-products__btn-mobile description-product__btn popup-btn">@lang('main.order')</button>
+      </div>
+
+      <div class="outdor-advertising-products__item item-outdor-advertising-products">
+         <div class="item-outdor-advertising-products__description description-product">
+            <h2 class="description-product__tittle"> @lang('polygraphy.products.folders.tittle')</h2>
+            <div class="description-product__body">
+               <div class="description-product__text">
+                  @lang('polygraphy.products.folders.text')
+               </div>
+               <div class="description-product__details">
+                  @lang('polygraphy.products.folders.details')
+               </div>
+               <button class="description-product__btn popup-btn">@lang('main.order')</button>
+            </div>
+         </div>
+         <div class="item-outdor-advertising-products__img">
+            <img src="{{asset('img/outdoor_advertising/lightboxes.jpg')}}" alt="">
+         </div>
+         <button
+            class="item-outdor-advertising-products__btn-mobile description-product__btn popup-btn">@lang('main.order')</button>
+      </div>
+
+      <div class="outdor-advertising-products__item item-outdor-advertising-products">
+         <div class="item-outdor-advertising-products__img">
+            <img src="{{asset('img/outdoor_advertising/oracal.jpg')}}" alt="">
+         </div>
+         <div class="item-outdor-advertising-products__description description-product">
+            <h2 class="description-product__tittle">
+               @lang('polygraphy.products.stickers.tittle')</h2>
+            <div class="description-product__body">
+               <div class="description-product__text">
+                  @lang('polygraphy.products.stickers.text')
+               </div>
+               <div class="description-product__type">@lang('polygraphy.products.stickers.types')</div>
+               <ul class="description-product__list products-list-read-more">
+                  <li class="description-product__item">@lang('polygraphy.products.stickers.item')</li>
+                  <li class="description-product__item">@lang('polygraphy.products.stickers.item1')</li>
+               </ul>
+               <div class="description-product__details">
+                  @lang('polygraphy.products.stickers.details')
+               </div>
+               <button class="description-product__btn popup-btn">@lang('main.order')</button>
+            </div>
+         </div>
+         <button
+            class="item-outdor-advertising-products__btn-mobile description-product__btn popup-btn">@lang('main.order')</button>
+      </div>
+
+      <div class="outdor-advertising-products__item item-outdor-advertising-products">
+         <div class="item-outdor-advertising-products__description description-product">
+            <h2 class="description-product__tittle">@lang('polygraphy.products.menu.tittle')</h2>
+            <div class="description-product__body">
+               <div class="description-product__text">
+                  @lang('polygraphy.products.menu.text')
+               </div>
+               <div class="description-product__details">
+                  @lang('polygraphy.products.menu.details')
+               </div>
+               <button class="description-product__btn popup-btn">@lang('main.order')</button>
+            </div>
+         </div>
+         <div class="item-outdor-advertising-products__img">
+            <img src="{{asset('img/outdoor_advertising/baner.jpg')}}" alt="">
+         </div>
+         <button
+            class="item-outdor-advertising-products__btn-mobile description-product__btn popup-btn">@lang('main.order')</button>
+      </div>
+
+      <div class="outdor-advertising-products__item item-outdor-advertising-products">
+         <div class="item-outdor-advertising-products__img">
+            <img src="{{asset('img/outdoor_advertising/letters.jpg')}}" alt="">
+         </div>
+         <div class="item-outdor-advertising-products__description description-product">
+            <h2 class="description-product__tittle">@lang('polygraphy.products.magazine.tittle')</h2>
+            <div class="description-product__body">
+               <div class="description-product__text">
+                  @lang('polygraphy.products.magazine.text')
+               </div>
+               <div class="description-product__details">
+                  @lang('polygraphy.products.magazine.details')
+               </div>
+               <button class="description-product__btn popup-btn">@lang('main.order')</button>
+            </div>
+         </div>
+         <button
+            class="item-outdor-advertising-products__btn-mobile description-product__btn popup-btn">@lang('main.order')</button>
+      </div>
+
+      <div class="outdor-advertising-products__item item-outdor-advertising-products">
+         <div class="item-outdor-advertising-products__description description-product">
+            <h2 class="description-product__tittle">@lang('polygraphy.products.certificate.tittle')</h2>
+            <div class="description-product__body">
+               <div class="description-product__text">
+                  @lang('polygraphy.products.certificate.text')
+               </div>
+               <div class="description-product__details">
+                  @lang('polygraphy.products.certificate.details')
+               </div>
+               <button class="description-product__btn popup-btn">@lang('main.order')</button>
+            </div>
+         </div>
+         <div class="item-outdor-advertising-products__img">
+            <img src="{{asset('img/outdoor_advertising/neon_signs.jpg')}}" alt="">
+         </div>
+         <button
+            class="item-outdor-advertising-products__btn-mobile description-product__btn popup-btn">@lang('main.order')</button>
+      </div>
+
+   </div>
+</section>
 @endsection
