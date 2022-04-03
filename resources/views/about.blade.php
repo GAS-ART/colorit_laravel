e76795dca6e59f1a7087341cf073280c8874545f
Artem <temoha1386@gmail.com>
2022-04-03 15:00:19 +0300

    play whith reviews styles

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -35,112 +35,169 @@
 <section class="review">
    <div class="review__container">
       <h2 class="review__tittle tittle">Отзывы</h2>
-      <div class="review__row">
-         <div class="review__items">
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Colorit agency - лучшее рекламное агентство в Барселоне.</p>
-                  <p>Работаем вместе уже 3 года, очень довольны.</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Вячеслав <span>¨Ecotown¨</span></p>
-               </div>
+      <div class="review__items">
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Colorit agency - лучшее рекламное агентство в Барселоне.</p>
+               <p>Работаем вместе уже 3 года, очень довольны.</p>
             </div>
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Я работала с ребятами по вывескам и винилу. Очень осталась довольна, ребята профессионалы,
-                     отзывчивы и быстро реагируют на просьбы! Рекомендую</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Анна salon de estetica <span>¨Melanta¨</span></p>
-               </div>
+            <div class="item-review__author">
+               <p>Вячеслав <span>¨Ecotown¨</span></p>
             </div>
          </div>
-         <div class="review__items">
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Приятно иметь дело с профессионалами! Большое спасибо за качество, быстрое исполнение и понимание!
-                  </p>
-               </div>
-               <div class="item-review__author">
-                  <p>Inesa <span>DUO special</span></p>
-               </div>
+         <div class="review__item item-review">
+            <div class="item-review__text item-price-smm">
+               <p>Я работала с ребятами по вывескам и винилу. Очень осталась довольна, ребята профессионалы,
+                  отзывчивы и быстро реагируют на просьбы! Рекомендую</p>
+            </div>
+            <div class="item-review__author">
+               <p>Анна salon de estetica <span>¨Melanta¨</span></p>
             </div>
          </div>
-         <div class="review__items">
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Gracias por vuestro colaboración y trabajo rápido y correcto!!! EQUIPO FLORATELIE</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Ksenia Chalaya <span>¨Floratelie¨</span></p>
-               </div>
+         <div class="review__item item-review">
+            <div class="item-review__text ">
+               <p>Приятно иметь дело с профессионалами! Большое спасибо за качество, быстрое исполнение и понимание!
+               </p>
             </div>
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Отличные и надежные ребята. Работа выполнено отлично и в срок! Рекомендую!</p>
-                  <p>Buen trabajo e buen servicio! Recomiendo!</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Alina Kabakova <span></span></p>
-               </div>
+            <div class="item-review__author">
+               <p>Inesa <span>DUO special</span></p>
             </div>
          </div>
       </div>
-      <div class="review__row">
-         <div class="review__items">
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Recomendable!!! lo repito hacer pedidos seguro! Trabajo excelente! Además me han hecho diseño que
-                     quería y ha quedado precioso Estoy muy contenta con el servicio y calidad Atención perfecta!!!
-                     Muchas gracias .</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Maria <span>"Studio Maria Zubko"</span></p>
-               </div>
+
+      <div class="review__items">
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Gracias por vuestro colaboración y trabajo rápido y correcto!!! EQUIPO FLORATELIE</p>
             </div>
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Заказывали футболки с логотипом. Качество просто супер. Рекомендую</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Tatiana <span>¨Gas Auto¨</span></p>
-               </div>
+            <div class="item-review__author">
+               <p>Ksenia Chalaya <span>¨Floratelie¨</span></p>
             </div>
          </div>
-         <div class="review__items">
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Работа выполнена качественно и в срок , Найдено техническое решение в невероятно сложной ситуации,
-                     настоящие профессионалы! Благодарна! Удачи вам и процветания!</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Anna <span>¨Laboratorio de estetica¨</span></p>
-               </div>
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Отличные и надежные ребята. Работа выполнено отлично и в срок! Рекомендую!</p>
+               <p>Buen trabajo e buen servicio! Recomiendo!</p>
+            </div>
+            <div class="item-review__author">
+               <p>Alina Kabakova <span></span></p>
             </div>
          </div>
-         <div class="review__items">
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Пользуемся услугами Colorit уже третий год, очень довольны качеством! Ответственные исполнители и
-                     отличная и качество печати. Спасибо</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Лидия <span>¨Li.Beauty Lounge¨</span></p>
-               </div>
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Recomendable!!! lo repito hacer pedidos seguro! Trabajo excelente! Además me han hecho diseño que
+                  quería y ha quedado precioso Estoy muy contenta con el servicio y calidad Atención perfecta!!!
+                  Muchas gracias .</p>
             </div>
-            <div class="review__item item-review">
-               <div class="item-review__text">
-                  <p>Мы сотрудничаем с компанией Colorit agency уже более 1,5 лет. Наша компания занимается декором и
-                     флористикой праздничных мероприятий и пространств, поэтому заказы наши были оооочень разными
-                     начиная от визиток и наклеек , заканчивая ростовыми фигурами и 42метровым баннером. Команда всегда
-                     на связи, относится с пониманием к экстренным заказам (а в нашей сфере форс-мажор дело привычное),
-                     дизайнеры слушают , а главное слышат все пожелания, в общем все Чётко, Качественно, Быстро и
-                     Своевременно. Рекомендуем</p>
-               </div>
-               <div class="item-review__author">
-                  <p>Svetlana <span>¨Globos bcn¨</span></p>
-               </div>
+            <div class="item-review__author">
+               <p>Maria <span>"Studio Maria Zubko"</span></p>
+            </div>
+         </div>
+      </div>
+
+
+      <div class="review__items">
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Заказывали футболки с логотипом. Качество просто супер. Рекомендую</p>
+            </div>
+            <div class="item-review__author">
+               <p>Tatiana <span>¨Gas Auto¨</span></p>
+            </div>
+         </div>
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Работа выполнена качественно и в срок , Найдено техническое решение в невероятно сложной ситуации,
+                  настоящие профессионалы! Благодарна! Удачи вам и процветания!</p>
+            </div>
+            <div class="item-review__author">
+               <p>Anna <span>¨Laboratorio de estetica¨</span></p>
+            </div>
+         </div>
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Пользуемся услугами Colorit уже третий год, очень довольны качеством! Ответственные исполнители и
+                  отличная и качество печати. Спасибо</p>
+            </div>
+            <div class="item-review__author">
+               <p>Лидия <span>¨Li.Beauty Lounge¨</span></p>
+            </div>
+         </div>
+      </div>
+
+      <div class="review__items">
+
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Мы сотрудничаем с компанией Colorit agency уже более 1,5 лет. Наша компания занимается декором и
+                  флористикой праздничных мероприятий и пространств, поэтому заказы наши были оооочень разными
+                  начиная от визиток и наклеек , заканчивая ростовыми фигурами и 42метровым баннером. Команда всегда
+                  на связи, относится с пониманием к экстренным заказам (а в нашей сфере форс-мажор дело привычное),
+                  дизайнеры слушают , а главное слышат все пожелания, в общем все Чётко, Качественно, Быстро и
+                  Своевременно. Рекомендуем</p>
+            </div>
+            <div class="item-review__author">
+               <p>Svetlana <span>¨Globos bcn¨</span></p>
+            </div>
+         </div>
+
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Excelente trabajo del diseño de imagen corporativa para nuestra tienda. Da gusto tratar con los
+                  profesionales. Convertiremos al cliente fijo sin duda! 100% recomendado!</p>
+            </div>
+            <div class="item-review__author">
+               <p>Diana <span>¨Bike Parts BCN¨</span></p>
+            </div>
+         </div>
+
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Все качественно и в обозначенные сроки! Хотим поблагодарить команду Colorit agency за предоставленные
+                  услуги в изготовлении подарочных сертификатов.</p>
+            </div>
+            <div class="item-review__author">
+               <p>Vitaliy <span>¨SV Detailing¨</span></p>
+            </div>
+         </div>
+      </div>
+
+      <div class="review__new-style">
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Colorit agency - лучшее рекламное агентство в Барселоне.</p>
+               <p>Работаем вместе уже 3 года, очень довольны.</p>
+            </div>
+            <div class="item-review__author">
+               <p>Вячеслав <span>¨Ecotown¨</span></p>
+            </div>
+         </div>
+         <div class="review__item item-review">
+            <div class="item-review__text item-price-smm">
+               <p>Я работала с ребятами по вывескам и винилу. Очень осталась довольна, ребята профессионалы,
+                  отзывчивы и быстро реагируют на просьбы! Рекомендую</p>
+            </div>
+            <div class="item-review__author">
+               <p>Анна salon de estetica <span>¨Melanta¨</span></p>
+            </div>
+         </div>
+      </div>
+      <div class="review__new-style">
+         <div class="review__item item-review">
+            <div class="item-review__text ">
+               <p>Приятно иметь дело с профессионалами! Большое спасибо за качество, быстрое исполнение и понимание!
+               </p>
+            </div>
+            <div class="item-review__author">
+               <p>Inesa <span>DUO special</span></p>
+            </div>
+         </div>
+         <div class="review__item item-review">
+            <div class="item-review__text">
+               <p>Gracias por vuestro colaboración y trabajo rápido y correcto!!! EQUIPO FLORATELIE</p>
+            </div>
+            <div class="item-review__author">
+               <p>Ksenia Chalaya <span>¨Floratelie¨</span></p>
             </div>
          </div>
       </div>
