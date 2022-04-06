254841e586d30a2664e2fb8a3dff49a98ef7f2b4
Artem <temoha1386@gmail.com>
2022-04-07 00:17:04 +0300

    Added translation for polygraphy page

---
+++ resources/lang/es/polygraphy.php
@@ -0,0 +1,78 @@
+<?php
+return [
+   'about' => '<span>Servicio de publicidad y impresión digital en Barcelona.</span> Agencia de publicidad y marketing Colorit agency realiza todos tipo de impresión digital y rotulación en España ofreciendo a sus clientes mejor soluciones para aumentar las ventas. Aqui os proponemos : tarjetas de visitas baratas, flyers y folletos rápidos, tripticos y dipticos, revistas de alta calidad, carpetas a4 para inmobiliarias, pegatinas de papel adhesivo, vinilo para rotulación y gran formato. La producción rápida con la entrega gratuita en Barcelona.',
+   'relevance' => [
+      'tittle' => '<span>Полиграфия</span> актуальна сегодня!',
+      '1' => '<p class="tittle-polygraphy-relevance" ><span>Полиграфия</span> актуальна сегодня!</p> <p>С приходом социальных сетей и интернет маркетинга многие думали что полиграфическая продукция больше не актуальна и сбрасывали ее со счетов. Но по последним исследованием доказано, что ведущие бренды, средний бизнес и начинающие предприниматели продолжают заказывать, распространять любые виды полиграфии.</p><p>Заказать визитки, флаера, буклеты либо папки - является отличным выходом рассказать о себе и своем бизнесе вашей целевой аудитории<br>Напечатать журналы, меню, презентации является обязательным условием для открытия ресторана либо же ежегодных выставок.</p>',
+      '2' => '<p>Заказать визитки, флаера, буклеты либо папки - является отличным выходом рассказать о себе и своем бизнесе вашей целевой аудитории</p>.<p>Напечатать журналы, меню, презентации является обязательным условием для открытия ресторана либо же ежегодных выставок.</p>',
+      '3' => '¿Para quién es recomendable la publicidad?<p>La agencia colorit agency realiza todos tipo de publicidad en Barcelona para cualquier negocio. Fabricamos las tarjetas de visita en Barcelona para abogados, tarjetas para salones de belleza, tarjetas de visita para centros de estetica y medicina, para los restaurantes y bares, para centros de deporte</p>',
+      '4' => 'Hacer el pedido para las tarjetas de visita, folletos y otros tipos de publicidad en España se puede en nuestra empresa.',
+   ],
+      'products' => 
+      [
+         'tittle' => 'Tipos de productos',
+         'business_cards' =>
+         [
+            'tittle' => 'Tarjetas de visita',
+            'text' => '<p>tamaño 85*55</p>',
+            'text_more' => '<ul><li>- Tarjetas con relieve</li><li>- Tarjetas con relieve 2D, 3D</li><li>- Tarjetas soft touch terciopelo</li><li>- Tarjetas de visita con el cartón especial</li><li>- Tarjetas de visita de papel craft</li><li>- Tarjetas de pvc</li><ul>',
+            'types' => 'Cual tarjetas fabricamos',
+            'item' => '- Tarjetas baratas estándar',
+            'item1' => '- Tarjetas plastificadas brillo/mate',
+            'item2' => '- Tarjetas letterpress bajo relieve',
+            'item3' => '- Tarjetas con relieve',
+            'item4' => '- Tarjetas con relieve 2D, 3D',
+            'item5' => '- Tarjetas soft touch terciopelo',
+            'item6' => '- Tarjetas de visita con el cartón especial',
+            'item7' => '- Tarjetas de visita de papel craft',
+            'item8' => '- Tarjetas de pvc',
+            'details' => '<p><span>Pedir las tarjetas de visita en Barcelona</span></p>',
+         ],
+         'flyers' => 
+         [
+            'tittle' => 'Los flyers',
+            'text' => '<p>Realizamos los folletos en diferentes tamaños: а6 (10*15).</p><p>euroformato (10*21)</p><p>formato а5 (15*21)</p><p>а4 формат (21*29).</p>',
+            'details' => '<p><span>Gramaje</span> de 130 hasta 210 gr.</p>',
+         ],
+         'booklet' => 
+         [
+            'tittle' => 'Dipticos y tripticos',
+            'text' => '<p>Los trípticos tienen un tamaño estándar а4</p><p>Dos tipos de pliegue U y Z.</p>',
+            'details' => '<p><span>Gramaje </span>de 130 hasta 170 gr.</p>',
+         ],
+         'folders' => 
+         [
+            'tittle' => 'Carpetas',
+            'text' => '<p>Fabricamos las carpetas estandart en tamaño a4.</p><p>Con solapa y sin</p><p>Con espacio para tarjeta de visita.</p><p>Laminación mate o brillo.</p>',
+            'details' => '<p><span>Gramaje de papel</span> de 300 hasta 350 gr</p>',
+         ],
+         'stickers' => 
+         [
+            'tittle' => 'Pegatinas y adhesivos',
+            'text' => '<p>Las pegatinas salen en rollo o tambien las fabricamos en ojas a3.</p>',
+            'types' => 'Tipos de pegatinas',
+            'item' => '- Pegatinas de polipropileno',
+            'item1' => '- vinilo o papel',
+            'details' => '<p><span>Tamaño</span> minimo es 3 cm de cualquier forma</p>',
+         ],
+         'menu' => 
+         [
+            'tittle' => 'Menus Cartas',
+            'text' => '<p>Fabricamos munes para restaurantes</p>',
+            'details' => '<p>De espirales o de grapas con cubierta dura</p><p><span>Gramaje de papel</span> de 150 hasta 350 gr</p><p><span>Laminación </span> mate o brillo</p>',
+         ],
+         'magazine' => 
+         [
+            'tittle' => 'Revistas',
+            'text' => '<p>Realizamos las revistas de alta gama</p>',
+            'details' => '<p>Con espirales o grapas</p><p><span>Gramaje de papel</span> de 150 hasta 350 gr</p><p><span>Laminación </span> mate o brillo</p>',
+         ],
+         'certificate' => 
+         [
+            'tittle' => 'Certificados',
+            'text' => '<p>Pedir certificados de regalo</p>',
+            'details' => '<p>Don el cartón especial</p><p><span>Gramaje</span> de 300 gr</p>',
+         ],
+      ],
+   ]
+?>
