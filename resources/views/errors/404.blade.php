edb31961b3a0fccf0021c4b6b8a4e3f616b31b4c
Artem <temoha1386@gmail.com>
2022-04-19 17:26:11 +0300

    Added translation for 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,7 +9,7 @@
 {{app()->setLocale('es')}}
 @endif
 @extends('layout')
-@section('title', __('tittles.contacts.title'))
+@section('title', '404')
 @section('description', __('tittles.contacts.description'))
 @section('ogTitle', __('tittles.contacts.ogTitle'))
 @section('ogDescription', __('tittles.contacts.ogDescription'))
@@ -20,13 +20,12 @@
 <section class="page-404">
    <div class="page-404__container">
       <div class="page-404__body">
-         <h1 class="page-404__tittle">404</h1>
+         <h1 class="page-404__tittle">@lang('404.tittle')</h1>
          <div class="page-404__text">
-            <p>Похоже, что такой страницы нет...</p>
+            <p>@lang('404.text')</p>
          </div>
          <div class="page-404__button">
-            <a href="{{ route('home.lang', ['locale' => __('main.set_lang')]) }}" class="button button-404">&#8656
-               Вернуться на сайт</a>
+            <a href="{{ route('home.lang', ['locale' => __('main.set_lang')]) }}" class="button button-404">@lang('404.link')</a>
          </div>
       </div>
    </div>
